45f5f5f1d8c2cf5cd9a04647c6c25545cc3a5dfc
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-09-15 09:41:50 -0500

    completed base instance for Task 2

--- index.js
+++ index.js
@@ -70,7 +70,12 @@ Person.prototype.toString = function () {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {}
+function Car(model, milesPerGallon) {
+  this.model = model
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0
+  this.mileage = 0
+}
 
 /*
   TASK 3
